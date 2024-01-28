f44af4a16464da2886286453103f0b5a94b63e27
IcaroJurado <icaaro.dias@gmail.com>
2024-01-28 15:04:37 -0300

    cliente api configuration

--- app/infrastructure/build.gradle.kts
+++ app/infrastructure/build.gradle.kts
@@ -3,5 +3,4 @@ dependencies {
 
     testImplementation("io.mockk:mockk:1.13.5")
     testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:4.12.0")
-    testImplementation(project(mapOf("path" to ":app:api")))
 }
