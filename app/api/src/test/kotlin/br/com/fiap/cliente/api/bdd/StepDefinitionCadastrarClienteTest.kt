4b8b5060db1b63c8696430c954667902597cbacd
IcaroJurado <icaaro.dias@gmail.com>
2024-01-29 20:51:57 -0300

    create test config to ClienteController

--- app/api/src/test/kotlin/br/com/fiap/cliente/api/bdd/StepDefinitionCadastrarClienteTest.kt
+++ app/api/src/test/kotlin/br/com/fiap/cliente/api/bdd/StepDefinitionCadastrarClienteTest.kt
@@ -10,31 +10,31 @@ import org.springframework.http.HttpStatus
 private const val ENDPOINT = "http://localhost:8080/clientes/cpf"
 private const val VALID_CPF = "287.081.130-68"
 
-class StepDefinitionCadastrarClienteTest {
+class StepDefinitionCadastrarClienteTest : CucumberTest() {
 
     lateinit var response: Response
 
     @When("eu solicitar o cadastro de um novo cliente")
     fun cadastrarUmCliente() {
-            response = given()
-                .contentType("application/json")
-                .body(
-                    """
+        response = given()
+            .contentType("application/json")
+            .body(
+                """
                         {
                             "cpf": $VALID_CPF,
                             "nome": "João da Silva",
                             "email": "teste@teste.com"
                             }
                             """.trimIndent()
-                )
-                .`when`()
-                .post(ENDPOINT)
-        }
+            )
+            .`when`()
+            .post(ENDPOINT)
+    }
 
     @Then("deve retornar o cliente cadastrado com sucesso")
     fun validarCadastroCliente() {
-            response.then()
-                .statusCode(HttpStatus.CREATED.value())
-                .body("cpf", equalTo(VALID_CPF))
-        }
+        response.then()
+            .statusCode(HttpStatus.CREATED.value())
+            .body("cpf", equalTo(VALID_CPF))
     }
+}
