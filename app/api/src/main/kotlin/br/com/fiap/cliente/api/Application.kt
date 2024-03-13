077ce48845cec47182e64ca4c2146335e459fb8d
IcaroJurado <icaaro.dias@gmail.com>
2024-03-12 21:01:29 -0300

    create test config to ClienteController

--- app/api/src/main/kotlin/br/com/fiap/cliente/api/Application.kt
+++ app/api/src/main/kotlin/br/com/fiap/cliente/api/Application.kt
@@ -1,10 +1,12 @@
 package br.com.fiap.cliente.api
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @SpringBootApplication
+@EnableRabbit
 @EnableMongoRepositories(basePackages = ["br.com.fiap.cliente.*"])
 class Application
 
